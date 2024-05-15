7c54051b69f09304b8e0fc76c7c19f3ddc2fc5e5
JOSEFRAN1980 <josefran.oliveira@gmail.com>
2024-05-15 12:11:01 -0300

    ESTRUTURAS DE REPETIÇÃO - FOR / WHILE / DO WHILE

--- ExemploFor/src/ExemploBreakContinue.java
+++ ExemploFor/src/ExemploBreakContinue.java
@@ -1,13 +1,23 @@
 public class ExemploBreakContinue {
     public static void main(String[] args) {
         
-        for(int numero = 1; numero <=5; numero ++){
+     /*    for(int numero = 1; numero <=5; numero ++){
             if(numero==3)
                 break;
+            
+            System.out.println(numero); 
+            
+        } */
+        //o comando break, parou quando chegou no número 3
+
+
+        for(int numero = 1; numero <=5; numero ++){
+            if(numero==3)
+                continue;
             
             System.out.println(numero);
             
         }
-        //o comando break, parou quando chegou no número 3
+        //no exemplo acima, usamos o comando "continue". Repare que programa executará até que chegue no número 5.
     }
 }
